ae40aa2449ddae9b22af45758c253364af8c6381
orkvacy <krotago@gmail.com>
2026-06-21 19:22:22 +0800

    Revert "feat(profil): tambah seksi galeri & leaflet map"

--- src/app/(profile)/page.tsx
+++ src/app/(profile)/page.tsx
@@ -1,8 +1,6 @@
 import Hero from "@/components/profile/Hero";
 import Reveal from "@/components/profile/Reveal";
 import Seal from "@/components/profile/Seal";
-import GallerySection from "@/components/profile/GallerySection";
-import ContactSection from "@/components/profile/ContactSection";
 import { ArrowRight, FileText, Store, Trees } from "@/components/profile/icons";
 
 const MISI = [
@@ -144,10 +142,6 @@ export default function BerandaPage() {
           </div>
         </div>
       </section>
-
-      <GallerySection />
-
-      <ContactSection />
     </>
   );
 }
